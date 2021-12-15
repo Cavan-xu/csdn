b12e5877bef13be893f997d05bdeeeb484b370d4
cavan.xu <2582099310@qq.com>
2021-12-15 16:01:41 +0800

    feature: add unsafe operation

--- unsafe/main.go
+++ unsafe/main.go
@@ -10,6 +10,10 @@ import (
 func main() {
 	operateVariable()
 	operateStruct()
+
+	data := StringToBytes("hello gopher")
+	str := BytesToString(data)
+	fmt.Println(str)
 }
 
 func operateVariable() {
@@ -20,10 +24,7 @@ func operateVariable() {
 	fmt.Println(ptr)
 
 	// 先将 *int64 类型转化为 *Arbitrary 类型再转化为 *int32类型
-	ptr = (*int32)(unsafe.Pointer(&f))
-	fmt.Println(ptr)
-
-	*ptr = 10
+	*(*int32)(unsafe.Pointer(&f)) = 10
 
 	fmt.Println(a)
 	fmt.Println(f)
@@ -35,12 +36,26 @@ func operateStruct() {
 	fmt.Printf("%+v\n", user)
 
 	// 突破第一个私有变量，因为是结构体的第一个字段，所以不需要额外的指针计算
-	p := (*string)(unsafe.Pointer(user))
-	*p = "张伟"
+	*(*string)(unsafe.Pointer(user)) = "张伟"
 	fmt.Printf("%+v\n", user)
 
 	// 突破第二个私有变量，因为是第二个成员字段，需要偏移一个字符串占用的长度即 16 个字节
-	ptrId := (*int)(unsafe.Pointer(uintptr(unsafe.Pointer(user)) + uintptr(16)))
-	*ptrId = 1
-	fmt.Printf("%+v", user)
+	*(*int)(unsafe.Pointer(uintptr(unsafe.Pointer(user)) + uintptr(16))) = 1
+	fmt.Printf("%+v\n", user)
+}
+
+func StringToBytes(str string) []byte {
+	var b []byte
+	// 切片的底层数组、len字段，指向字符串的底层数组，len字段
+	*(*string)(unsafe.Pointer(&b)) = str
+
+	// 切片的 cap 字段赋值为 len(str)的长度
+	*(*int)(unsafe.Pointer(uintptr(unsafe.Pointer(&b)) + 2*uintptr(8))) = len(str)
+
+	return b
+}
+
+func BytesToString(data []byte) string {
+	// 直接转化
+	return *(*string)(unsafe.Pointer(&data))
 }
